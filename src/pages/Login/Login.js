5ab3e3343ff781d9e6203afaf26ed3539fc07395
Andrew Ellender <aelle14@lsu.edu>
2022-11-27 14:40:33 -0600

    login css fix

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -66,43 +66,45 @@ function Login(){
 
 
         return(
+<body className="bodylogin">
 <div className="container" id="container">
 	<div className="overlay-container">
 		<div className="overlay">
 			<div className="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button className='ghost' id="signIn">Sign In</button>
+				<h1 className="h1login">Welcome Back!</h1>
+				<p className="paralogin">To keep connected with us please login with your personal info</p>
+				<button className='buttonlogin ghost' id="signIn">Sign In</button>
 			</div>
 			<div className="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button className='ghost' id="signUp">Sign Up</button>
+				<h1 className="h1login">Hello, Friend!</h1>
+				<p className="paralogin">Enter your personal details and start journey with us</p>
+				<button className='buttonlogin ghost' id="signUp">Sign Up</button>
 			</div>
 		</div>
 	</div>
     <div className="form-container sign-up-container">
-		<form action="#">
-			<h1>Create Account</h1>
-			<span>or use your email for registration</span>
-					<input type = 'First Name' name = 'Fname' placeholder = 'First Name' required onChange={(e)=>setFnameReg(e.target.value)}/>
-                    <input type = 'Last Name' name = 'Lname' placeholder = 'Last Name' required onChange={(e)=>setLnameReg(e.target.value)}/>
-                    <input type = 'email' name = 'email' placeholder = 'email...' required onChange={(e)=>setEmailReg(e.target.value)}/>
-                    <input type = 'password' name = 'pwd' placeholder = 'password...' required onChange={(e)=>setPasswordReg(e.target.value)}/>
-			<button onClick={register}>Register</button>
+		<form className="formlogin" action="#">
+			<h1 className="h1login">Create Account</h1>
+			<span className="spanlogin">or use your email for registration</span>
+					<input className="inputlogin" type = 'First Name' name = 'Fname' placeholder = 'First Name' required onChange={(e)=>setFnameReg(e.target.value)}/>
+                    <input className="inputlogin" type = 'Last Name' name = 'Lname' placeholder = 'Last Name' required onChange={(e)=>setLnameReg(e.target.value)}/>
+                    <input className="inputlogin" type = 'email' name = 'email' placeholder = 'email...' required onChange={(e)=>setEmailReg(e.target.value)}/>
+                    <input className="inputlogin" type = 'password' name = 'pwd' placeholder = 'password...' required onChange={(e)=>setPasswordReg(e.target.value)}/>
+			<button className="buttonlogin" onClick={register}>Register</button>
 		</form>
 	</div>
 	<div className="form-container sign-in-container">
-		<form action="#">
+		<form className="formlogin" action="#">
 		<h1 className="wrongep">{loginStatus}</h1>
-			<h1>Sign in</h1>
-			<span>or use your account</span>
-			<input type='email' name='email' placeholder='email...' required onChange={(e)=>setEmail(e.target.value)} />
-			<input type='password' name='pwd' placeholder='password...' required onChange={(e)=>setPassword(e.target.value)}/>
-			<button onClick={Loginn}>Login In</button>
+			<h1 className="h1login">Sign in</h1>
+			<span className="spanlogin">or use your account</span>
+			<input className="inputlogin" type='email' name='email' placeholder='email...' required onChange={(e)=>setEmail(e.target.value)} />
+			<input className="inputlogin" type='password' name='pwd' placeholder='password...' required onChange={(e)=>setPassword(e.target.value)}/>
+			<button className="buttonlogin" onClick={Loginn}>Login In</button>
 		</form>
 	</div>
 </div>
+</body>
 
         )
 }
